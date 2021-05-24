db05129d0bf0ebb41e3a914db8b4ba755554b233
MD Muftee <mahfuzul.muftee@tum.de>
2021-05-24 16:25:05 +0000

    setup eslint and prettier

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,11 +1,10 @@
-import express from "express";
-import {signup, login} from "../controller/auth/auth";
+import express from "express"
+import { signup, login } from "../controller/auth/auth"
 
+const router = express.Router()
 
-const router = express.Router();
+router.post("/playerSignup", signup)
 
-router.post('/playerSignup', signup);
+router.post("/playerLogin", login)
 
-router.post('/playerLogin', login);
-
-export default router;
+export default router
