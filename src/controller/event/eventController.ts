e197d1a5c23720c32fd250bdf1ba7bcd0ae5ab69
Irtiza Chowdhury <ge24mej@mytum.de>
2021-07-07 20:42:30 +0200

    add event revenue

--- src/controller/event/eventController.ts
+++ src/controller/event/eventController.ts
@@ -302,6 +302,7 @@ export const updateRegistered = async (
   try {
     const userId = getUserId(req)
     const registered = req.body.registered
+    const fee = req.body.fee
     const player = await PlayerModel.findById(userId)
     const event = await EventModel.findById(req.params.id)
 
@@ -309,6 +310,7 @@ export const updateRegistered = async (
       if (registered) {
         player.registeredEvents.push(event._id)
         event.registeredPlayers.push(player._id)
+        event.revenue += fee
       } else {
         player.registeredEvents = player.registeredEvents.filter(
           (item) => item != event._id
@@ -316,6 +318,8 @@ export const updateRegistered = async (
         event.registeredPlayers = event.registeredPlayers.filter(
           (item) => item != userId
         )
+        event.revenue -= fee
+        event.revenue < 0 ? (event.revenue = 0) : event.revenue
       }
 
       const notify = await createNotification(
